749c93a1bfe1cdcee9871ec70a5d700631bf019e
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-16 19:07:34 +0600

    Update name_val.php

--- Lab task 1/name_val.php
+++ Lab task 1/name_val.php
@@ -4,10 +4,22 @@
         <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
             <fieldset>
                 <legend>NAME</legend>
-                <input type="text" id="name" name="name" pattern="[\s]" size="30"><br>
+                <input type="text" id="name" name="name" pattern="[\s]" size="30" ><br>
                 <hr>
                 <input type="submit" name="submit" value="Submit">
             </fieldset>
         </form>
+        <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+          $name = $_POST['name'];
+          if (empty($name)) 
+          {
+            echo "Email is empty";
+          } else {
+            echo "Your Email is: $name ";
+          }
+        }
+        ?>
     </body>
 </html>
