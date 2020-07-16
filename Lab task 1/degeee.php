2ccb5a08093de8fb42c4f3b1724b610012fac3c8
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-16 16:09:00 +0600

    Update degeee.php

--- Lab task 1/degeee.php
+++ Lab task 1/degeee.php
@@ -31,7 +31,6 @@
         <?php
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
-                
                 if(isset($_POST['submit']))
                 {
                     if(!empty($_POST["check"]))
