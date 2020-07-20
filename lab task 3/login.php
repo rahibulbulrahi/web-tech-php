6027ba2aba49fe6e7dad1995e3581da4252d38f2
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-20 10:33:58 +0600

    done

---
+++ lab task 3/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .a label
+        {
+            width: 80px;
+            display: inline-block;
+            text-align: right;
+        }
+    </style>
+    <body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <fieldset>
+            <legend style="font-family: calibri;">LOG IN</legend>
+            <div class="a">
+                <label for="uname">User name:</label>
+                <input type="text" id="uname" name="uname"><br>
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="password"><br>
+                </div>
+                <hr>
+                <input type="checkbox" name="check" value="Remember Me">
+                <span class="checkmark"></span>
+                <label class="addition_check">Remember Me
+                </label>
+                <br><br>
+                <input type="submit" value="Submit">
+                <a href="">forget password?</a>
+            
+        </fieldset>
+        </form>
+        <br>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+          $uname = $_POST['uname'];
+          $password = $_POST['password'];
+          if (empty($uname || $password)) 
+          {
+            echo "Username or Passowrd is empty";
+          } else {
+            echo "Your Usename is $uname and password is $password";
+          }
+        }
+        ?>
+    </body>
+</html>
