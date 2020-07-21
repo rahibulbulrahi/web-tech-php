e64a727db6210e4f8df034dda04b2860cfa1a427
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-21 09:47:55 +0600

    1

--- lab task 3/profile_picture.php
+++ lab task 3/profile_picture.php
@@ -4,61 +4,30 @@
 
     </style>
     <body>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-        </form>
+        <fieldset>
+        <legend style="font-family: calibri;">PROFILE PICTURE</legend>
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+            <input type="file" name="file" id="file">
+            <hr>
+            <input type="submit" value="Upload Image" name="submit">
+            </form>
         
+        </fieldset>
         <?php
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            if(isset($_POST['submit']))
+            {
+                $file = $_FILES['file'];
+                
+                $fileName = $_FILES['file']['name'];
+                $fileTmpName = $_FILES['file']['tmp_name'];
+                $fileSize = $_FILES['file']['size'];
+                $fileError = $_FILES['file']['error'];
+                $fileType = $_FILES['file']['type'];
 
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-        }
-
-        // Check if file already exists
-        if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-        }
-
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-        }
-
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-        } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-        }
+                $fileEXT = explode('.', $fileName);
+                $fileActualExt = strtolower(end($fileEXT));
+                
+            }
         ?>
-
     </body>
 </html>
