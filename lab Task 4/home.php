acf706791e9734fb0568061ae32db6b8f799a9f0
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-22 23:23:06 +0600

    7

--- lab Task 4/home.php
+++ lab Task 4/home.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
     <style>
-    
+        body
+        {
+            background: lightgray;
+        }
     </style>
 
     <body>
     <?php include('header.php');?>
 
-    
+
 
     <?php include('footer.php');?>
     </body>
