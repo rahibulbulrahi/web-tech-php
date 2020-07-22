485aa4f9baf4bf69deeb0b1403ac606798b085e0
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-23 00:02:28 +0600

    9

--- lab Task 4/home.php
+++ lab Task 4/home.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
-   
     <style>
         body
         {
             background: lightgray;
             width: 100%;
             text-align: justify;
-            
+        }
+        hr.line_2 
+        {
+            border: 5px solid Grey;border-radius: 5px;width: 90%;
         }
     </style>
     <body>
     <?php include('header.php');?>
-        <h1>Welcome to our website</h1>
+    <h1 style="font-size: 40px;text-align: center;">
+            <pre>W E L C O M E  T O  E A S Y  T O U R  &  T R A V E L S</pre>
+            <hr class="line_2">
+            <p style="font-size: 20px;translate: 7%;">
+        </h1>
     <?php include('footer.php');?>
     </body>
 </html>
