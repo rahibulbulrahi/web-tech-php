6425e0afb5b0d21841fe94ad7b4eeece0d39c667
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-25 00:32:25 +0600

    6

---
+++ lab Task 4/forgetpass.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        body
+        {
+            background: lightgray;
+        }
+    </style>
+
+    <body>
+    <?php include('header.php');?>
+
+
+
+    <?php include('footer.php');?>
+    </body>
+</html>
