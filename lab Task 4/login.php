9aece6c12b735e4be8953f7cc0a367822b546374
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-25 00:36:40 +0600

    7

--- lab Task 4/login.php
+++ lab Task 4/login.php
@@ -5,13 +5,37 @@
         {
             background: lightgray;
         }
+        form
+        {
+            width: 50%;
+            margin: auto;
+        }
+        div.login
+        {
+            font-weight: Bold;
+        }
+        .login label
+        {
+            width: 120px;
+            display: inline-block;
+            text-align: left;
+            margin: 3px;
+        }
     </style>
 
     <body>
     <?php include('header.php');?>
+    <form id="login" >
+            <fieldset style="border: black 2px solid;">
+                <legend><b>LOG IN</b></legend>
 
+                <div class="login">
+                    <label for="uname">User Name</label>
+                    <input type="text" id="uname" name="uname" size="55px" placeholder="name*" required>
 
-
+                </div>
+            </fieldset>
+        </form>
     <?php include('footer.php');?>
     </body>
 </html>
