401be3aa77a16ff1f51779e2452b40052d76d017
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-24 11:14:25 +0600

    2

--- lab Task 4/registration.php
+++ lab Task 4/registration.php
@@ -10,7 +10,25 @@
             width: 50%;
             margin: auto;
         }
-
+        .registration label
+        {
+            width: 150px;
+            display: inline-block;
+            text-align: left;
+            margin: 3px;
+        }
+        input[type="text"]
+        {
+            overflow: hidden;
+            font-size: 15px;
+            padding: 5px 0;
+            border: none;
+            outline: none;
+            background: none;
+            color:black;
+            border-bottom: 1px solid black;
+            
+        }
     </style>
     <body>
     <?php include('header.php');?>
@@ -20,19 +38,19 @@
                 <div class="registration">
 
                 <label for="name">Name</label>
-                : <input type="text" id="name" name="name" required><br>
+                : <input type="text" id="name" name="name" size="50px" placeholder="name*" required><br>
 
                 <label for="email">Email</label>
-                : <input type="text" id="email" name="email" required><br>
+                : <input type="text" id="email" name="email" size="50px" placeholder="email*" required><br>
 
                 <label for="uname">User name:</label>
-                : <input type="text" id="uname" name="uname" required><br>s
+                : <input type="text" id="uname" name="uname" size="50px" placeholder="user name*" required><br>
 
                 <label for="password">Password:</label>
-                : <input type="text" id="password" name="password" required><br>
+                : <input type="text" id="password" name="password" size="50px" placeholder="password*" required><br>
 
                 <label for="cpassword">User name:</label>
-                <input type="text" id="cpassword" name="cpassword" required><br>
+                : <input type="text" id="cpassword" name="cpassword" size="50px" placeholder="confirm password*" required><br>
                 </div>
             </fieldset>
         </form>
