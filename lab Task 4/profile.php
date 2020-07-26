8a67f87414365ab3251efc8ac4513cdfa030577e
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-26 20:40:49 +0600

    4

--- lab Task 4/profile.php
+++ lab Task 4/profile.php
@@ -11,11 +11,37 @@
             margin: auto;
             padding: 50px;
         }
+        div.sidebar
+        {
+            position: fixed;
+            width: 250px;
+            height: 100%;
+            background: #b3b3b3;
+        }
+        .sidebar ul li
+        {
+            font-size: 15px;
+            color: black;
+            list-style: none;
+            padding: 15px 15px;
+            border-bottom: 2px solid grey;
+        }
     </style>
 
     <body>
     <?php include('header.php');?>
-
+        
+        <div class="sidebar">
+            <ul>
+                <li>Dashboard</li>
+                <li>View Profie</li>
+                <li>Edit Profile</li>
+                <li>Change Profile Picture</li>
+                <li>Change Profile Password</li>
+                <li>Log Out</li>
+            </ul>
+        </div>
+    
     <?php include('footer.php');?>
     </body>
 </html>
