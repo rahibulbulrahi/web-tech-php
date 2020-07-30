ad3ca177de75f76ad68324f4c925644841b83fc8
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-07-30 10:27:44 +0600

    10

--- Lab task 5/controller/createpackage.php
+++ Lab task 5/controller/createpackage.php
@@ -9,26 +9,21 @@ if(isset($_POST['createpackage']))
     $data['price'] = $_POST['price'];
     $data['image'] = basename($_FILES["image"]["name"]);
 
-    $target_dir = "../uploads/";
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) 
-    {
-        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-    } 
-    else 
-    {
-        echo "Sorry, there was an error uploading your file.";
-    }
-    
-    if (addpackage($data)) 
-    {
-          echo 'Successfully added!!';
-    }
-    else 
-    {
-        echo 'You are not allowed to access this page.';
-    }
+	$target_dir = "../uploads/";
+	$target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+	if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+
+  if (addStudent($data)) {
+  	echo 'Successfully added!!';
+  }
+} else {
+	echo 'You are not allowed to access this page.';
+}
 }
 
 ?>
