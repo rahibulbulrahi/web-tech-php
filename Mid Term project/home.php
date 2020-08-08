7bbfa03a72c9294238c240cb56e67d9acab8034b
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-08 11:34:33 +0600

    5

--- Mid Term project/home.php
+++ Mid Term project/home.php
@@ -7,9 +7,23 @@
     <body>
     <?php include('header.php');?>
         
-    <div id="section-1" class="section">
-        <img src="images/banner.png" alt="">
-    
+    <div class="slides">
+        <input type="radio" name="r" id="r1" checked>
+        <input type="radio" name="r" id="r2" >
+        <input type="radio" name="r" id="r3" >
+    </div>
+    <div class="slideshow">
+        <div class="slide-1">
+            <img src="images/1.png" alt="">
+        </div>
+        
+        <div class="slide-2">
+            <img src="images/2.png" alt="">
+        </div>
+
+        <div class="slide-3">
+            <img src="images/3.png" alt="">
+        </div>
     </div>
     
     <?php include('footer.php');?>
