58cf7c22ee54f1b83dfcea57095643b0fc2dc919
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-08 23:44:53 +0600

    6

---
+++ ADMS/registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Food Delivery</title>
+    </head>
+    <style> 
+        body
+        {
+            margin: 0px;
+            font-family: Calibri;
+            font-weight: 400;
+            background: #f2f2f2;
+        }
+    </style>
+    <body>
+        <?php include("header.php")?>
+
+    </body>
+</html>
