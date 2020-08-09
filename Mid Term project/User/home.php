ddef85d1ecb7a54b5bbabc7e79c39027de9ab215
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-09 19:01:22 +0600

    3

--- Mid Term project/User/home.php
+++ Mid Term project/User/home.php
@@ -8,9 +8,31 @@
             padding: 0;
             background: #fff;
         }
+        input[type="search"]
+        {
+            overflow: hidden;
+            font-size: 15px;
+            padding: 5px 0;
+            border: none;
+            outline: none;
+            background: none;
+            color:black;
+            border-bottom: 1px solid black;
+        }
+        div.find
+        {
+            background: #b3ffe6;
+            width: 90%;
+            height: 300px;
+            margin: auto;
+            border-radius: 12px;
+        }
         div.search
         {
-            
+            font-family: 'cooper';
+            width: 50%;
+            margin: auto;
+            padding: 60px 10%;
         }
         
     </style>
@@ -18,9 +40,15 @@
     <body>
     <?php include('header.php');?>
     
-    <div class="search">
-    <h1>FIND THE GOOD TRI HERE</h1>
+    <div class="find">
+    <form>
+        <div class="search">
+            <h1><b>FIND THE GOOD TRIP HERE</b></h1>
+            <input type="search" id="search" name="search" size="60px" placeholder="Where to go?*" required><br>
+        </div>
+    </form>
     </div>
+    
         
     
 
