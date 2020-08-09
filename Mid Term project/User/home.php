b2ee209ac34f7c310763619d9aff222f816f36aa
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-09 18:35:56 +0600

    2

--- Mid Term project/User/home.php
+++ Mid Term project/User/home.php
@@ -8,14 +8,19 @@
             padding: 0;
             background: #fff;
         }
-        
+        div.search
+        {
+            
+        }
         
     </style>
 
     <body>
     <?php include('header.php');?>
     
-    
+    <div class="search">
+    <h1>FIND THE GOOD TRI HERE</h1>
+    </div>
         
     
 
