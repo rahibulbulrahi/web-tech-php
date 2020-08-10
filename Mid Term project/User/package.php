5e8d1fcb74f3ca20e9a7fb4572942848033538e1
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-10 10:24:36 +0600

    .

--- Mid Term project/User/package.php
+++ Mid Term project/User/package.php
@@ -11,12 +11,57 @@
         div.main
         {
             width: 80%;
-            height: 450px;
+            height: 500px;
             position: relative;
             margin: 100px auto;
         }
-        
-        
+        div.content
+        {
+            padding: 20px 10%;
+        }
+        div.btn-box
+        {
+            display: flex;
+            border-bottom: 1px solid #ccc;
+        }
+        .btn-box button
+        {
+            background: transparent;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            padding: 0 10px;
+            margin-right: 50px;
+            font-size: 20px;
+        }
+        div.international
+        {
+            width: 250px;
+            height: 200px;
+            display: grid;
+            grid-template-rows: auto;
+            grid-template-columns: 250px 250px 250px;
+            grid-gap: 50px;
+        }
+        div.kualalumpur
+        {
+            background: lightblue;
+            text-align: center;
+        }
+        div.singapore
+        {
+            background: lightblue;
+            text-align: center;
+        }
+        div.dubai
+        {
+            background: lightblue;
+            text-align: center;
+        }
+        a
+        {
+            text-decoration: none;
+        }
     </style>
 
     <body>
@@ -27,7 +72,25 @@
             <button>International</button>
             <button>Domestic</button>
         </div>
+        
+        <div class="content">
+            <div class="international">
+                <div class="kualalumpur">
+                    <a href="">Kuala Lumpur</a>
+                </div>
+                <div class="singapore">
+                    <a href="">Singapore</a>
+                </div>
+                <div class="dubai">
+                    <a href="">Dubai</a>
+                </div>
+            </div>
+            <div class="domestic">
+
+            </div>
+        </div>
     </div>
+    
    
     
     <?php include('footer.php');?>
