7698c60b486e90dc2787ec67b6ca4962c7fa5c14
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-10 08:59:40 +0600

    2

--- Mid Term project/User/register.php
+++ Mid Term project/User/register.php
@@ -5,15 +5,92 @@
         {
             margin: 0;
             padding: 0;
-            background: #fff;
+            background: #efefef;
+            font-size: 16px;
+            color: #777;
+            font-family: sans-serif;
+            font-weight: 300;
         }
-
+        #login-box
+        {
+            position: relative;
+            margin: 5% auto;
+            width: 600px;
+            height: 400px;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.6);
+        }
+        div.left-box
+        {
+            position: absolute;
+            top: 0;
+            left: 0;
+            box-sizing: border-box;
+            padding: 40px;
+            width: 300px;
+            height: 400px;
+        }
+        h1
+        {
+            margin: 0 0 20px 0;
+            font-weight: 300;
+            font-size: 28px;
+        }
+        input[type="text"],input[type="password"]
+        {
+            display: block;
+            box-sizing: border-box;
+            margin-bottom: 20px;
+            padding: 4px;
+            width: 220px;
+            height: 32px;
+            border: none;
+            outline: none;
+            border-bottom: 1px solid #aaa;
+            font-family: sans-serif;
+            font-weight: 400;
+            font-size: 15px;
+            transition: 0.2s ease;
+        }
+        input[type="submit"]
+        {
+            margin-bottom: 28px;
+            width: 120px;
+            height: 32px;
+            background: #f44336;
+            border: none;
+            border-radius: 2px;
+            color: #fff;
+            font-family: sans-serif;
+            font-weight: 500;
+            text-transform: uppercase;
+            transition: 0.2s ease;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover,input[type="submit"]:focus
+        {
+            background: #ff5722;
+            transition: 0.2s ease;
+        }
+        div.right-box
+        {
+            position: absolute;
+            top: 0;
+            right: 0;
+            box-sizing: border-box;
+            padding: 40px;
+            width: 300px;
+            height: 400px;
+            background-image: url(1.png);
+            background-size: cover;
+            background-position: center;
+        }
     </style>
 
     <body>
     <?php include('header.php');?>
 
-        <div id="loginin-box">
+        <div id="login-box">
             <div class="left-box">
                 <h1>Sign Up</h1>
 
@@ -31,6 +108,7 @@
                 <button class="social twitter">Log in with Twitter</button>
                 <button class="social google">Log in with Google+</button>
             </div>
+            <div class="or">OR</div>
         </div>
 
     <?php include('footer.php');?>
