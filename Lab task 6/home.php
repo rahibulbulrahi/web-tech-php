f51276322a3b72720fcac2d6c1e65bf7e9928643
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-19 08:50:02 +0600

    3 (home complete)

--- Lab task 6/home.php
+++ Lab task 6/home.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html>
+    <style>
+        body
+        {
+            margin: 0px;
+            font-family: Calibri;
+            font-size: 50px;
+            text-align: center;
+        }
+    </style>
     <body>
         <?php include('header.php');?>
+
+        <h1>Welcome to our website</h1>
     </body>
 </html>
