1f62151544a492edf062edda7d4c901e646c7f0c
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-27 15:36:09 +0600

    7

--- Lab Task 7/home.php
+++ Lab Task 7/home.php
@@ -1,10 +1,67 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="style/homestyle.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"> 
         <title>Home Page</title>
     </head>
+
     <body>
         <?php include('header.php')?> 
-        This is home page
+        <div class="find">
+    <form>
+        <div class="search">
+            <h1><b>FIND THE GOOD TRIP HERE</b></h1>
+            <input type="search" id="search" name="search" size="60px" placeholder="Where to go?*" required>
+            <input class="button" type="submit" value="Search" name="search">
+        </div>
+    </form>
+    </div>
+    
+    <div class="about">
+        <h1>About US</h1>
+        <hr class="line"><br>
+    </div>
+    
+    <div class="about_us">
+        <div class="destination">
+            
+        <h3>Best Destination</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco l
+                aboris nisi ut aliquip ex ea commodo consequat.<p>
+        
+        </div>
+        
+        <div class="trust">
+      
+        <h3>Trust & Safety</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco l
+                aboris nisi ut aliquip ex ea commodo consequat.<p>
+        </div>
+    </div>
+    
+    <div class="tour">
+        <h1>Top Tour</h1>
+        <hr class="line"><br>
+    </div>
+        
+    <div class="trip">
+        <div class="domestic">
+            <h3>For Domestic tour visit <a href="package.php">Here</a></h3>
+        </div>
+
+        <div class="international">
+            <h3>For International tour visit <a href="package.php">Here</a></h3>
+        </div>
+        
+        <div class="package_tour">
+            <h3>For Package tour Program visit <a href="package.php">Here</a></h3>
+        </div>
+    </div>
     </body>
 </html>
--- Lab Task 7/login.php
+++ Lab Task 7/login.php
@@ -6,25 +6,33 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="style/login.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"> 
-        <title>Login</title>
+        <title>Log in</title>
     </head>
     <body>
         <div class="main">
             <div class="form">
                 <h2>Sign in</h2>
                 <label>
-                    <span>Email Address</span>
-                    <input type="email" name="email">
+                    <input type="email" placeholder="Email Address*" name="email">
                 </label>
 
                 <label>
-                    <span>Password</span>
-                    <input type="password" name="password">
+                    <input type="password" placeholder="Password*" name="password">
                 </label>
                 <button class="submit" type="button">Sign in</button>
                 <p class="forget-pass">Forget password?</p>
+
+                <div class="social-media">
+                    <ul>
+                        <li><img src="images/facebook.png"></li>
+                        <li><img src="images/twitter.png"></li>
+                        <li><img src="images/linkedin.png"></li>
+                        <li><img src="images/facebook.png"></li>
+                    </ul>
+                </div>    
             </div>
-        </div>
+      
+        </div>   
     </body>
 
 </html>
