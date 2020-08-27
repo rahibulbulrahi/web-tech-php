f18d05beaadb86d70f9af92209b13c27fede91a3
Rahi Bulbul <66356154+rahibulbulrahi@users.noreply.github.com>
2020-08-27 14:52:46 +0600

    .

---
+++ Lab Task 7/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="style/login.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"> 
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="main">
+            <div class="form">
+                <h2>Sign in</h2>
+                <label>
+                    <span>Email Address</span>
+                    <input type="email" name="email">
+                </label>
+
+                <label>
+                    <span>Password</span>
+                    <input type="password" name="password">
+                </label>
+                <button class="submit" type="button">Sign in</button>
+                <p class="forget-pass">Forget password?</p>
+            </div>
+        </div>
+    </body>
+
+</html>
